0d80a12ea9c328d85c0dbaccabc7ecb8f702fe6a
Mekhazni <168568735+Mekhazni@users.noreply.github.com>
2026-08-20 16:29:19 +0100

    Implement BMI calculation and categorization functions

---
+++ Python-Task2-BMICalculator/advanced/bmi_logic.py
@@ -0,0 +1,27 @@
+def calculate_bmi(weight, height):
+    return weight / (height ** 2)
+
+
+def get_bmi_category(bmi):
+    if bmi < 18.5:
+        return "Underweight"
+
+    elif bmi < 25:
+        return "Normal"
+
+    elif bmi < 30:
+        return "Overweight"
+
+    else:
+        return "Obese"
+
+
+def get_category_color(category):
+    colors = {
+        "Underweight": "orange",
+        "Normal": "green",
+        "Overweight": "orange",
+        "Obese": "red"
+    }
+
+    return colors.get(category, "black")
